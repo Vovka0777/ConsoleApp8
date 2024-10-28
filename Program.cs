6d99935329ec4100e6366695bd9d17c40c3a8ecb
vovab <vovab@MSTRBG>
2024-10-28 21:01:15 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задача 1.Заполнить одномерный массив из 8 целых чисел с клавиатуры и вывести его на экран.
+
+            //int[] nums = new int[8];
+            //for (byte i = 0; i < 8; i++) {Console.WriteLine("Введите символ массива - "); nums[i]=byte.Parse(Console.ReadLine());}
+            //for (byte i = 0; i < 8; i++) { Console.Write($"{nums[i]}, "); }
+            //Console.Read();
+
+            //Задача 2. Составьте программу ввода (без клавиатуры) и вывода массива: 5, -2, 3, -6, 7, 9, -4, 8, 1, 2, 7, -3.
+
+            //int[] nums = new int[] { 5, -2, 3, -6, 7, 9, -4, 8, 1, 2, 7, -3 };
+            //for (byte i = 0; i < 12; i++) { Console.Write($"{nums[i]}, "); }
+            //Console.Read();
+
+            //Задача 3.Массив предназначен для хранения значений ростов двенадцати человек.С помощью датчика случайных чисел заполнить массив целыми значениями, лежащими в диапазоне от 163 до 190 включительно.
+
+            //int[] nums = new int[12];
+            //Random yyy = new Random();
+            //for (int i = 0; i < nums.Length; i++) { nums[i] = yyy.Next(163,190); Console.WriteLine($"{nums[i]}"); }
+            //Console.Read();
+        }
+    }
+}
